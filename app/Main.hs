2962c2163ec12a2fd5fdfee37563258e54346c6f
Andreas Ekeroot <andeke@gmail.com>
2017-08-31 00:48:29 +0200

    Added an import tool for agendas. This closes #3.

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,6 @@ import Control.Concurrent.STM.TVar (newTVarIO)
 import Control.Exception (bracket)
 import Network.Wai.Logger
 import Network.Wai.Handler.Warp
-import System.Directory
 
 import Lib
 import qualified DB
@@ -13,23 +12,10 @@ import qualified DB
 dbFilename :: String
 dbFilename = "lemmingpants.db"
 
--- | Dies horribly if database can't be loaded.
-loadOrDie :: IO DB.Database
-loadOrDie = do
-    exists <- doesFileExist dbFilename
-    if exists
-       then do
-            db <- DB.load dbFilename
-            case db of
-              Left  s   -> error s
-              Right db' -> return db'
-       else
-            return DB.empty
-
 main :: IO ()
 main = bracket
     ( do
-        db <- loadOrDie
+        db <- DB.loadOrDie dbFilename
         v1 <- newTVarIO db
         v2 <- newBroadcastTChanIO
         return (v1, v2)
