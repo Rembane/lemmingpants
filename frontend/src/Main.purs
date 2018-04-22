633b6e1038a845262b6ef1689f80057ef1aea438
Andreas Ekeroot <andeke@gmail.com>
2018-04-23 00:03:50 +0200

    Fixed bugs regarding getting tokens. Refactored and cleaned.

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -1,14 +1,11 @@
 module Main where
 
-import Data.Tuple.Nested
-
 import Control.Apply (lift3)
 import Control.Coroutine as CR
 import Control.Monad.Aff (Aff, forkAff, liftEff', parallel, sequential)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (throw)
 import Control.Monad.Eff.Now (now)
 import Coroutines (consumerToQuery, routeProducer)
 import DOM.Websocket.WebSocket as WS
@@ -20,8 +17,8 @@ import Data.Foreign (Foreign, MultipleErrors, renderForeignError)
 import Data.Int (toNumber)
 import Data.Maybe (maybe)
 import Data.Time.Duration (Milliseconds(..))
+import Data.Tuple.Nested (Tuple3, get1, get2, get3, tuple3)
 import Data.Validation.Semigroup (V, invalid, unV)
-import Debug.Trace (traceA)
 import Effects (LemmingPantsEffects)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
@@ -55,20 +52,17 @@ combineFailures e1 e2 e3 =
     fromV = unV Left Right
   in fromV (lift3 tuple3 (toV e1) (toV e2) (toV e3))
 
-loadInitialState :: forall e. Aff (LemmingPantsEffects e) LP.Input
+loadInitialState :: forall e. Aff (LemmingPantsEffects e) (Either MultipleErrors LP.Input)
 loadInitialState = do
-  rs <- sequential
+  sequential
     (combineFailures <$>                                        parallel loadOrGetNewToken
                      <*> ((\r -> parseAgenda    r.response) <$> parallel (AX.get initialAgendaUrl))
                      <*> ((\r -> parseAttendees r.response) <$> parallel (AX.get initialAttendeesUrl)))
-  case rs of
-    Left  es  ->
-      traceA (foldMap renderForeignError es) *> liftEff' (throw "FETCH DATA ERROR!")
-    Right tpl ->
-      pure { token:                           get1 tpl
-           , agenda:    AG.jumpToFirstActive (get2 tpl)
-           , attendees: newAttendeeDB        (get3 tpl)
-           }
+    <#> rmap (\tpl ->
+      { token:                           get1 tpl
+      , agenda:    AG.jumpToFirstActive (get2 tpl)
+      , attendees: newAttendeeDB        (get3 tpl)
+      })
   where
     initialAgendaUrl = PG.createURL "/agenda_item?select=*,speakerQueues:speaker_queue(id,state,speakers:active_speakers(id,attendeeId:attendee_id,state,timesSpoken:times_spoken))&speaker_queue.state=in.(init,active)&order=order_.asc&order=speaker_queue.id.asc&speaker_queue.active_speakers.state=in.(init,active)"
     initialAttendeesUrl = PG.createURL "/attendee?select=id,cid,name,nick,numbers:attendee_number(id)"
@@ -79,29 +73,20 @@ loadInitialState = do
     loadOrGetNewToken = do
       et <- loadToken
       case et of
-        Left  _ ->
-          PG.requestAnonymousToken
-            <#> \t -> rmap (saveToken) t *> t
+        Left  _ -> do
+          et' <- PG.requestAnonymousToken
+          case et' of
+            Left  l -> (pure <<< Left) l
+            Right r -> do
+              _ <- saveToken r
+              (pure <<< pure) r
         Right t@(Token t') ->
           (liftEff' now) >>= \n ->
             let (Payload p) = t'.payload
-                mexp        = instant (Milliseconds (toNumber (p.exp * 1000)))
+                mexp        = instant (Milliseconds ((toNumber p.exp) * 1000.0))
              in if maybe false (_ > n) mexp
                   then (pure <<< pure) t
                   else removeToken *> loadOrGetNewToken -- The recursion hack!
-
-main :: forall e. Eff (LemmingPantsEffects (console :: CONSOLE | e)) Unit
-main = do
-  log "Hello lemming!"
-  HA.runHalogenAff do
-    body       <- HA.awaitBody
-    st         <- loadInitialState
-    connection <-
-      let (Token t') = st.token
-       in liftEff $ WS.create (WS.URL $ PG.createWSURL $ "/state_updates/" <> t'.raw) []
-    driver     <- runUI LP.component st body
-    pfest (wsProducer connection)      (consumerToQuery driver.query LP.WSMsg)
-    pfest (routeProducer LP.locations) (consumerToQuery driver.query LP.ChangePage)
 
 pfest
   :: forall a e
@@ -109,3 +94,20 @@ pfest
   -> CR.Consumer a (Aff (LemmingPantsEffects e)) Unit
   -> Aff           (LemmingPantsEffects e) Unit
 pfest a = void <<< forkAff <<< CR.runProcess <<< CR.connect a
+
+main :: forall e. Eff (LemmingPantsEffects (console :: CONSOLE | e)) Unit
+main = do
+  log "Hello lemming!"
+  HA.runHalogenAff do
+    body <- HA.awaitBody
+    est  <- loadInitialState
+    case est of
+      Left  es -> liftEff $ log $ "ERROR: " <> foldMap renderForeignError es
+      Right st -> do
+        connection <-
+          let (Token t') = st.token
+           in liftEff $ WS.create (WS.URL $ PG.createWSURL $ "/state_updates/" <> t'.raw) []
+        driver     <- runUI LP.component st body
+        pfest (wsProducer connection)      (consumerToQuery driver.query LP.WSMsg)
+        pfest (routeProducer LP.locations) (consumerToQuery driver.query LP.ChangePage)
+
