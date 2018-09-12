ab52bd7ae651bb01056c4b6be767db57faaf3ee1
Andreas Ekeroot <andeke@gmail.com>
2018-09-12 22:28:57 +0200

    Well

--- deployment/files/create_secret.sh
+++ deployment/files/create_secret.sh
@@ -1,13 +1,12 @@
 set -e
-PATH=/setup/secret_key.txt
-if [ ! -f $PATH ]; then
+FP=/setup/secret_key.txt
+if [ ! -f $FP ]; then
   CHARS=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
   RESULT=""
   for _ in {1..50}; do
     RESULT=$RESULT${CHARS:RANDOM%${#CHARS}:1}
   done
-  echo $RESULT > $PATH
+  echo $RESULT > $FP
 fi;
-KEY=`cat $PATH`
-psql -d lemmingpants < ALTER DATABASE lemmingpants SET "app.jwt_secret" TO "'$KEY'"
-
+KEY=`cat $FP`
+psql -d lemmingpants <<< 'ALTER DATABASE lemmingpants SET "app.jwt_secret" TO "'$KEY'"'
--- pg_tests/load-db.sh
+++ pg_tests/load-db.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# set -eu
 #
 # $1 is the name of the database.
 
-dropdb $1
+set -eu
+
+dropdb --if-exists $1
 createdb $1
 cd ..
 psql -d $1 < init.sql
