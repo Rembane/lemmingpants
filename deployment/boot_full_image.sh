dcbbee6fb16e56a2591e7923dd2b08cc0552d97b
hallavad <hallavad@users.noreply.github.com>
2019-04-11 14:20:41 +0200

    Made container names more descriptive

--- deployment/boot_full_image.sh
+++ deployment/boot_full_image.sh
@@ -24,14 +24,14 @@ if [[ $# -ne 4 ]]; then
     exit 1
 fi
 
-TAG='custom-lp-deployment-tag'
+TAG='dhackwuzhere18'
 if docker image ls | awk '{print $2}' | grep -q "$TAG"; then
     echo "Found an image with tag $TAG, maybe it's already running like you want it to, and all is fine. Otherwise delete it and try again."
 else
     docker run lemmingpants/backend-full:latest sed -i -e "s%http://localhost:3000%${3}%" -e "s%ws://localhost:8000%${4}%" /srv/static/main.js
     CONTAINER_ID=$(docker ps -lq)
     IMAGE_ID=$(docker commit $CONTAINER_ID | cut -c8-)
-    docker tag $IMAGE_ID lp/deployed-full:"$TAG"
+    docker tag $IMAGE_ID lemmingpants/deployed-full:"$TAG"
     docker run --restart always -d -p 127.0.0.1:"$1":3000 -p 127.0.0.1:"$2":8000 lp/deployed-full:"$TAG" /usr/local/bin/run_backend.sh
     echo "The container should now be online..."
 fi
